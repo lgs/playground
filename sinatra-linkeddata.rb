68d5fbbd61afd062f75496ab88b4afd435a2ccf0
Luca G. Soave <luca.soave@gmail.com>
2010-10-31 19:55:18 +0100

    sinatra + linkeddata RDF, see http://github.com/datagraph/sinatra-linkeddata

---
+++ sinatra-linkeddata.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby -rubygems
+# this come from: http://github.com/datagraph/sinatra-linkeddata
+require 'sinatra/base'
+require 'sinatra/linkeddata'
+
+module My
+  class Application < Sinatra::Base
+    register Sinatra::LinkedData
+
+    get '/hello' do
+      RDF::Graph.new do |graph|
+        graph << [RDF::Node.new, RDF::DC.title, "Hello, world!"]
+      end
+    end
+  end
+end
+
+My::Application.run! :host => '127.0.0.1', :port => 4567
+
